58a94f9f6131b93c9b4a5ab7e27831944f385a28
SrpMtt <srpmtt@gmail.com>
2015-12-22 17:25:28 +0100

    Test no longer requires user input

--- samples/TestScript.py
+++ samples/TestScript.py
@@ -7,89 +7,36 @@ import os
 consMan = ConsensusManager.ConsensusManager()
 consMan.bootstrap("http://127.0.0.1:8181")
 
+
 walletA = WalletExplorer.WalletExplorer(wallet_file='A.wallet')
 walletB = WalletExplorer.WalletExplorer(wallet_file='B.wallet')
-
 fManA = ForumManager.ForumManager(consMan, wallet=walletA)
 fManB = ForumManager.ForumManager(consMan, wallet=walletB)
 
 
 # A creates a new post
-try:
-   print('A posted: ', fManA.createPost('Hello post', 'test post'))
-except:
-   print('Error post A')
-   
-
-time.sleep(10)
-
+postid = fManA.createPost('Hello post', 'test post')
+consMan.waitBlock()
 
 # A gets the posts list
-while True:
-   os.system('clear')
-   print('Post list:')
-   posts = fManA.getList()
-   for p in posts:
-      print('PostID:', p['postID'], '\nTitle:', p['title'], '\nBody:', p['body'], '\nComments:', p['comments'], '\n')
-   wait = input('Refresh list? (y/n): ')
-   if wait == 'y':
-      pass
-   else:
-      break
-
-
+fManA.getList()       
 time.sleep(10)
 
-
-# A comments his post
-try:
-   postid = input('Insert post ID: ')
-   print('A commented: ', fManA.createComment(postid, 'This is a comment'))
-except:
-   print('Error comment A')
-
-
-time.sleep(10)
-
+# A comments his post                                                           
+commid = fManA.createComment(postid, 'This is a comment')
+consMan.waitBlock()
 
 # A gets post info
-while True:
-   os.system('clear')
-   print('Post info:')
-   posts = fManA.getList()
-   for p in posts:
-      print('PostID:', p['postID'], '\nTitle:', p['title'], '\nBody:', p['body'], '\nComments:', p['comments'], '\n')
-   wait = input('Refresh list? (y/n): ')
-   if wait == 'y':
-      pass
-   else:
-      fManA.getPost(input('Enter post ID: '))
-      break
-
-
+fManA.getPost(postid)
 time.sleep(10)
-
 
 # B creates a new post
-try:
-   print('B posted: ', fManB.createPost('Hello post 2', 'Post di test 2'))
-except:
-   print('Error post B')
-
-
+postid2 = fManB.createPost('Hello post 2', 'test post 2')
 time.sleep(10)
-
 
 # B comments A's post
-try:
-   postid = input('Insert post ID: ')
-   print('B commented: ', fManB.createComment(postid, 'This is a comment of B'))
-except:
-   print('Error comment B')
-
-
-time.sleep(10)
-
+commid2 = fManB.createComment(postid, 'This is a comment of B')
+consMan.waitBlock()
 
 # B gets posts list
 while True:
@@ -98,3 +45,4 @@ while True:
    posts = fManB.getList()
    for p in posts:
       print('PostID:', p['postID'], '\nTitle:', p['title'], '\nBody:', p['body'], '\nComments:', p['comments'], '\n')
+   time.sleep(10)
